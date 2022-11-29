2cecb4eccc7d2b1b1ca930a07b2b381fc6c6b9c6
TejasSudarshan <89351830+TejasSudarshan@users.noreply.github.com>
2022-11-29 18:06:40 +0530

    Update receiver_buffer.v

--- receiver_buffer.v
+++ receiver_buffer.v
@@ -1,16 +1,19 @@
 `timescale 1ns/1ps
+`define BITWIDTH 8
+
+
 module buffer_r( Clk, dataIn,RD, WR, rpaddr, dataOut, Rst,EMPTY); 
 
 input  Clk,RD,WR,Rst;
 input [1:0]rpaddr;
 output  EMPTY; 
 
-input [7:0]dataIn;
-output reg[7:0] dataOut; 
+input [`BITWIDTH-1:0]dataIn;
+output reg[`BITWIDTH-1:0] dataOut;
 
 assign EMPTY = (RD && !Rst)? 1'b1:1'b0;  
 
-reg[0:7]mem[3:0];
+reg[`BITWIDTH-1:0]mem[3:0];
 
 always @(posedge Clk)
 begin
@@ -22,7 +25,7 @@ else if(RD && !WR)
 dataOut=mem[rpaddr];
 
 else
-dataOut=0;
+dataOut=8'b0;
 end
 end
 
