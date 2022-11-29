29c1cdb5a80668fd81bb91abcc4a4d532007aa92
TejasSudarshan <89351830+TejasSudarshan@users.noreply.github.com>
2022-11-29 17:30:43 +0530

    Update transmitter_buffer.v

--- transmitter_buffer.v
+++ transmitter_buffer.v
@@ -1,20 +1,19 @@
 `timescale 1ns/1ps
-module buffer_t( tClk, tdataIn,tRD, tWR, tpaddr, tdataOut,/*tbidir,*/ tRst,tEMPTY,ttxrdy); 
+`define BITWIDTH 8
+module buffer_t( tClk, tdataIn,tRD, tWR, tpaddr, tdataOut,tRst,tEMPTY,ttxrdy); 
 
 input  tClk,tRD,tWR,tRst;
 input [1:0]tpaddr;
 output  tEMPTY,ttxrdy; 
 
-input [7:0]tdataIn;
-output reg[7:0] tdataOut;  
+input [`BITWIDTH-1:0]tdataIn;
+output reg[`BITWIDTH-1:0] tdataOut;   
 
 assign tEMPTY = (!tWR && tRD)? 1'b0:1'b1; 
- assign ttxrdy = (!tWR && tRD)? 1'b1:1'b0;
+ assign ttxrdy = (!tWR && tRD)? 1'b0:1'b1;
  
 
-reg [7:0]dout;
-
-reg[0:7]mem[3:0];
+reg[`BITWIDTH-1:0]mem[3:0];
 
 always @(posedge tClk)
 begin
