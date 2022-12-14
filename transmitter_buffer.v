75c10ca8e694352db7f7ef695bc14c62cec54475
TejasSudarshan <89351830+TejasSudarshan@users.noreply.github.com>
2022-12-14 09:30:22 +0530

    Update transmitter_buffer.v

--- transmitter_buffer.v
+++ transmitter_buffer.v
@@ -2,15 +2,14 @@
 `define BITWIDTH 8
 module buffer_t( tClk, tdataIn,tRD, tWR, tpaddr, tdataOut,tRst,tEMPTY,ttxrdy); 
 
-input  tClk,tRD,tWR,tRst;
-input [1:0]tpaddr;
-output  tEMPTY,ttxrdy; 
+ input  tClk,tRD,tWR,tRst;
+ input [1:0]tpaddr;
+ output  tEMPTY,ttxrdy; 
+ input [`BITWIDTH-1:0]tdataIn;
+ output reg[`BITWIDTH-1:0] tdataOut;   
 
-input [`BITWIDTH-1:0]tdataIn;
-output reg[`BITWIDTH-1:0] tdataOut;   
-
-assign tEMPTY = (!tWR && tRD)? 1'b0:1'b1; 
- assign ttxrdy = (!tWR && tRD)? 1'b1:1'b0;
+ assign tEMPTY = (!tWR && tRD)? 1'b0:1'b1; //buffer empty
+ assign ttxrdy = (!tWR && tRD)? 1'b1:1'b0; //data buffer status
  
 
 reg[`BITWIDTH-1:0]mem[3:0];
@@ -18,17 +17,14 @@ reg[`BITWIDTH-1:0]mem[3:0];
 always @(posedge tClk)
 begin
 if(tRst)begin
-if(tWR && !tRD)begin 
-mem[tpaddr]=tdataIn;
-
-end
+  if(tWR && !tRD)begin 
+   mem[tpaddr]=tdataIn;       //write to buffer
+  end
 else if(tRD)begin
-tdataOut=mem[tpaddr];
-
+ tdataOut=mem[tpaddr];      //Read from buffer
 end
-
 else
-tdataOut=0;
+   tdataOut=0;
 end
 end
 
