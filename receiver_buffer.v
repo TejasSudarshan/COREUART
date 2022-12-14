0eff35720a4dfbacea380f3e9d79622c6b7b240e
TejasSudarshan <89351830+TejasSudarshan@users.noreply.github.com>
2022-12-14 09:32:22 +0530

    Update receiver_buffer.v

--- receiver_buffer.v
+++ receiver_buffer.v
@@ -11,21 +11,19 @@ output  EMPTY;
 input [`BITWIDTH-1:0]dataIn;
 output reg[`BITWIDTH-1:0] dataOut;
 
-assign EMPTY = (RD && !Rst)? 1'b1:1'b0;  
+  assign EMPTY = (RD && !Rst)? 1'b1:1'b0;  //buffer empty status
 
 reg[`BITWIDTH-1:0]mem[3:0];
 
 always @(posedge Clk)
 begin
-if(!Rst)begin
-if(WR && !RD) 
-mem[rpaddr]=dataIn;
-
-else if(RD && !WR)
-dataOut=mem[rpaddr];
-
+ if(!Rst)begin
+   if(WR && !RD) 
+     mem[rpaddr]=dataIn;     //write to buffer
+ else if(RD && !WR)
+     dataOut=mem[rpaddr];     //Read from buffer
 else
-dataOut=8'b0;
+     dataOut=8'b0;
 end
 end
 
