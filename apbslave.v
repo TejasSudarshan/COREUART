8ee2087da4027cbe762af0f96b09124e6820974f
TejasSudarshan <89351830+TejasSudarshan@users.noreply.github.com>
2022-12-20 18:46:56 +0530

    Update apbslave.v

--- apbslave.v
+++ apbslave.v
@@ -30,7 +30,9 @@ parameter BITWIDTH=8;
 reg [BITWIDTH-1:0] mem[0:3];
 
 reg[1:0] state,next_state;
-
+	
+reg rxout = 1'b1;
+	
 assign TX_RDY = (tf_TXRDY)? 1'b0:1'b1;  //TX_RDY shows wether the buffer is empty or not
 assign RX_RDY = (rbuff_RXRDY)? 1'b1:1'b0; //RX_RDY shows the valid data available
 
