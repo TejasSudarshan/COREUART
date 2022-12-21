7df598a100c001e264c5789e40adf70ccf17c86b
TejasSudarshan <89351830+TejasSudarshan@users.noreply.github.com>
2022-12-21 10:03:28 +0530

    Update transmitter_buffer.v

--- transmitter_buffer.v
+++ transmitter_buffer.v
@@ -9,7 +9,7 @@ module buffer_t( tClk, tdataIn,tRD, tWR, tpaddr, tdataOut,tRst,tEMPTY,ttxrdy);
  output reg[`BITWIDTH-1:0] tdataOut;   
 
  assign tEMPTY = (!tWR && tRD)? 1'b0:1'b1; //buffer empty
- assign ttxrdy = (!tWR && tRD)? 1'b0:1'b1; //data buffer status
+ assign ttxrdy = (!tRst)? 1'b0:1'b1; //data buffer status
  
 
 reg[`BITWIDTH-1:0]mem[3:0];
